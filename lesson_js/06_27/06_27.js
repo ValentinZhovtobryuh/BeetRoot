6783c72f7c5d1ca6074e5c49774086b9708ea11e
Valentin Zhovtobryuh <valentyn@ukr.net>
2019-12-12 17:27:51 +0200

    les27  made simpl and complicated mas

--- lesson_js/06_27/06_27.js
+++ lesson_js/06_27/06_27.js
@@ -1,24 +1,5 @@
 
-
-
-let anchovies = {
-    name: "анчоусы",
-    amount: 10,
-    required: 5,
-    units: "grams",
-    // needToBuy: (anchovies.amount - anchovies.required),
-};
-
-let needToBuy = 0;
-
-// if  anchovies.amount == Number || (anchovies[amount] - anchovies[required]) > 0 {
-//     anchovies.needToBuy = 0;
-// } else { anchovies.needToBuy = anchovies[key] - anchovies[key] }
-
-
-
-
-var fish = [[anchovies, { Amount: 5, Required: 10 }], "calamari", "crab", "prawn", "bream", "salmon", "mussels", "seafood", "lobster", "octopus", "cod", "tuna", "oysters", "trout", "hake"];
+var fish = ["anchovies", "calamari", "crab", "prawn", "bream", "salmon", "mussels", "seafood", "lobster", "octopus", "cod", "tuna", "oysters", "trout", "hake"];
 
 var meat = ["lamb", "steak", "beef", "turkey", "sausage", "chicken", "pork", "veal"];
 
@@ -28,18 +9,38 @@ var FruitBerriesAndNuts = ["apricot", "avocado", "pineapple", "orange", "peanut"
 
 var shopping = [fish, meat, vegetables, FruitBerriesAndNuts];
 
-// function RequiredAmount(a) {
-//     for (let requir of a) {
-//         requir.push("Name", "Amount", "Required", "NeedToBuy");
-//     }
-// }
+function creatureMas(a, req, amo) {
+  for (i = 0; i < a.length; i++)
+    a[i] = {
+      name: a[i],
+      req: req,
+      amo: amo,
+      by: Boolean,
+    };
+  return;
+};
 
-// RequiredAmount(fish);
+function NeedToBay(name) {
+  for (i = 0; i < name.length; i++)
+    name[i].by = ((name[i].req - name[i].amo) > 0) ? true : false;
+  return;
+};
+
+
 console.log(shopping);
-console.log(fish[0]);
 
-  // if (requir.Amount - requir.Required) > 0 {
-    //     alert(enough);
-    // } else {
-    //     alert("need to buy" + (Math.abs(Amount - Required)));
-    // }
+creatureMas(fish, 4, 6);
+NeedToBay(fish);
+
+creatureMas(meat, 10, 5);
+NeedToBay(meat);
+
+creatureMas(vegetables, 3, 1);
+creatureMas(FruitBerriesAndNuts, 2, 2);
+
+console.log(fish[0]);
+console.log(fish[0].name);
+console.log(fish[0].req);
+console.log(fish[0].amo);
+console.log(fish[0].by);
+
